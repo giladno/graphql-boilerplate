62d0f10f36da9930a05fd47920193cb1878a9ead
Gilad Novik <gilad@novik.ca>
2019-11-23 20:52:03 +0200

    express based server

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 'use strict';
+const log = require('loglevel');
+const express = require('express');
 
-const server = require('./app')(require('apollo-server'));
+const server = require('./app')(require('apollo-server-express'), ({req, res}) => ({req, res}));
 
-server.listen().then(({url}) => {
-    console.log(`🚀 Server ready at ${url}`);
+const app = express();
+app.set('x-powered-by', false);
+
+server.applyMiddleware({app});
+
+app.use((req, res) => {
+    log.warn('not found', req.url);
+    res.status(404).end();
 });
+
+app.use((err, req, res, next) => {
+    log.error(req.url, err);
+    res.status(500).end();
+    next;
+});
+
+process.on('unhandledRejection', err => {
+    log.error(err);
+    throw err;
+});
+
+require('http')
+    .Server(app)
+    .listen(Number(process.env.PORT) || 3000, function() {
+        log.info(`server is running on port ${this.address().port}`);
+    })
+    .on('error', err => log.error(err));
